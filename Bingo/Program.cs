b2afe62ff502c8438de40835c480a238a085536f
Daniel Grondin <king@numsgil.co>
2019-06-18 14:02:33 -0400

    Fixed the free (card center)

--- Bingo/Program.cs
+++ Bingo/Program.cs
@@ -2,34 +2,30 @@
 
 namespace Bingo
 {
-	enum Colonne
-	{
-		B = 0,
-		I = 1,
-		N = 2,
-		G = 3,
-		O = 4
-	}
-
 	struct Case
 	{
-		public static Case Generate(Colonne colonne)
+		public static Case Generate(byte colonne)
 		{
 			return new Case(colonne, (uint) (
-				colonne == Colonne.B ? R.Next(1,  15) :
-				colonne == Colonne.I ? R.Next(16, 30) :
-				colonne == Colonne.N ? R.Next(31, 45) :
-				colonne == Colonne.G ? R.Next(46, 60) :
+				colonne == 0 ? R.Next(1,  15) :
+				colonne == 1 ? R.Next(16, 30) :
+				colonne == 2 ? R.Next(31, 45) :
+				colonne == 3 ? R.Next(46, 60) :
 				R.Next(61, 75)));
 		}
 
-		public Case(Colonne colonne, uint numero)
+		public Case(byte colonne, uint numero)
 		{
 			Colonne = colonne;
 			Numero = numero;
 		}
 
-		public readonly Colonne Colonne;
+		public override string ToString()
+		{
+			return Colonne == 2 && Numero == 0 ? "X" : Numero.ToString();
+		}
+
+		public readonly byte Colonne;
 		public readonly uint Numero;
 		
 		private static readonly Random R = new Random();
@@ -37,17 +33,17 @@ namespace Bingo
 
 	class Carte
 	{
-		public uint this[Colonne colonne, int ligne] => Cases[(int) colonne, ligne].Numero;
+		public Case this[byte colonne, int ligne] => Cases[colonne, ligne];
 
 		public readonly Case[,] Cases = new Case[5, 5];
 
 		public Carte()
 		{
-			for (int i = 0; i < 5; ++i)
+			for (byte i = 0; i < 5; ++i)
 				for (int j = 0; j < 5; ++j)
-					Cases[j, i] = Case.Generate((Colonne) i);
+					Cases[j, i] = Case.Generate(i);
 
-			Cases[2, 2] = new Case(Colonne.N, 0);
+			Cases[2, 2] = new Case(2, 0);
 		}
 
 		public void Print(int left, int top)
@@ -59,15 +55,14 @@ namespace Bingo
 			Console.SetCursorPosition(left, top);
 			Console.Write("╔══╦══╦══╦══╦══╗");
 			Console.SetCursorPosition(left, top + 1);
-			Console.Write("║B ║I ║N ║G ║O ║");
+			Console.Write("║ B║ I║ N║ G║ O║");
 			Console.SetCursorPosition(left, top + 2);
 			Console.Write("╠══╬══╬══╬══╬══╣");
 
-			for (int i = 0; i < 5; ++i)
+			for (byte i = 0; i < 5; ++i)
 			{
-				Colonne c = (Colonne) i;
 				Console.SetCursorPosition(left, top + 3 + i);
-				Console.Write("║{0,2}║{1,2}║{2,2}║{3,2}║{4,2}║", this[c, 0], this[c, 1], this[c, 2], this[c, 3], this[c, 4]);
+				Console.Write("║{0,2}║{1,2}║{2,2}║{3,2}║{4,2}║", this[i, 0], this[i, 1], this[i, 2], this[i, 3], this[i, 4]);
 			}
 			
 			Console.SetCursorPosition(left, top + 8);
